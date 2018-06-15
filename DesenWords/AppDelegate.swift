70affaecb78c9c770ef408f3d8b3f67e5d1383fc
cyyyu <cyu9960@gmail.com>
2018-06-15 15:29:55 +0800

    Initial Commit

---
+++ DesenWords/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DesenWords
+//
+//  Created by Chuang Yu on 15/6/2018.
+//  Copyright © 2018 Chuang Yu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
